43caf630ff365f51d2c6711cd4b8967d8bc97573
Kaiserrollii <64233714+Kaiserrollii@users.noreply.github.com>
2020-05-14 23:07:06 -0400

    Add new background tasks

--- Cogs/Background_Tasks_Cog.py
+++ Cogs/Background_Tasks_Cog.py
@@ -1,6 +1,8 @@
 import discord
 import asyncio
 import random
+import requests
+from bs4 import BeautifulSoup
 from discord.ext import commands 
 
 
@@ -8,32 +10,50 @@ class Background_Tasks_Cog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.bg_task = self.bot.loop.create_task(self.chat_revive())
+        self.bg_task = self.bot.loop.create_task(self.status())
+        self.bg_task = self.bot.loop.create_task(self.daily_affirmation())
+        self.bg_task = self.bot.loop.create_task(self.daily_bible())
 
     @commands.Cog.listener()
     async def on_ready(self):
         print ('Background_Tasks: Online')
 
-    # Chat Reviver: sends a randomized message into #the-lounge every 90 minutes
-    # Suggest new responses in #suggestions or via DM
-    async def chat_revive(self):
+    async def status(self):
         await self.bot.wait_until_ready()
-        channel = self.bot.get_channel(623770537340174336)
+        channel = self.bot.get_channel(630633322686578689)
+        while not self.bot.is_closed():
+            await channel.send('**Status:** Online')
+            await asyncio.sleep(3600) 
+
+    # Sends a daily affirmation into #the-spa every 24h
+    async def daily_affirmation(self):
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(627970443361648685)
         while not self.bot.is_closed():
 
-            revive_list = ['Excuse me, sir, but do you have a moment to talk about our lord and saviour: Irene?',
-                           'If you want 10000000000 months of free Nitro, please react to this message.',
-                           'I dislike burgers.',
-                           'I will go anywhere with anyone as long as there is sushi involved. Well, mostly anywhere. There are a few exceptions.',
-                           '_deadchat or no?',
-                           'Sushi is pog.',
-                           '_whoasked',
-                           "Don't be a simp.",
-                           "Don't forget to check the Epic store for this week's free games.",
-                           'sAAII MMorEE liKKEE cRRYY']
+            url = 'https://www.developgoodhabits.com/positive-affirmations/'
+            user_agent = {'User-Agent': 'Mozilla/5.0'}
 
-            await channel.send(f'{random.choice(revive_list)}')
-            await asyncio.sleep(5400) 
+            response = requests.get(url, headers = user_agent)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            affirms = list(soup.find_all('em'))
+            text = list(map(lambda x: x.text, affirms))
+
+            await channel.send(f'{random.choice(text)}')
+            await asyncio.sleep(86400) 
+
+    async def daily_bible(self):
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(627970443361648685)
+        while not self.bot.is_closed():
+
+            url = requests.get('https://www.verseoftheday.com/')
+            soup = BeautifulSoup(url.text, 'html.parser')
+            verse = soup.find('div', {'class': 'bilingual-left'})
+            formatted = ''.join(verse.text.replace('—', '\n- '))
+
+            await channel.send(f'**Daily Bible Verse:**\n{formatted}')
+            await asyncio.sleep(86400)
 
 
 def setup(bot):
