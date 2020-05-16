8c3047cc20c9a779af51dd12bf0a064e826ea7da
Kaiserrollii <64233714+Kaiserrollii@users.noreply.github.com>
2020-05-16 16:20:19 -0400

    Remove unneeded kill command

--- Main.py
+++ Main.py
@@ -18,15 +18,6 @@ async def on_ready():
     activity = discord.Activity(name = 'Interstellar 🔭', type = discord.ActivityType.watching)
     return await bot.change_presence(activity = activity)
 
-@bot.command(aliases = ['Kill', 'end', 'End', 'exit', 'Exit'])
-async def kill(ctx):
-    if ctx.message.author.id == 496181635952148483:
-        await ctx.send('**Status:** Offline\n**Background Tasks:** Offline\n**Data:** Offline\n**Events:** Offline\n**Games:** Offline\
-        \n**Images:** Offline\n**Math:** Offline\n**Miscellaneous:** Offline\n**Moderation:** Offline\n')
-        await bot.logout()
-    else:
-        await ctx.send("Sorry, but you're not the bot owner.")
-
 @bot.command(aliases = ['Cog_online', 'cogon'])
 async def cog_online(ctx, extension):
     if ctx.message.author.id == 496181635952148483:
